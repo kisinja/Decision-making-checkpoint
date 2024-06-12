e00faf0310a3a2c60d7b132fa56f992c78721b4e
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-06-12 15:45:29 +0300

    Changes to be committed: 	modified:   ticket pricing/app.js 	modified:   ticket pricing/index.html 	modified:   ticket pricing/style.css

--- ticket pricing/app.js
+++ ticket pricing/app.js
@@ -6,15 +6,21 @@ const reasonContainer = document.getElementById("reason-result");
 const checkPrice = () => {
     const ageValue = age.value;
 
+    if (ageValue === '') {
+        result.innerHTML = '$0';
+        reasonContainer.innerHTML = "Please enter your age";
+        return;
+    }
+
     if (ageValue >= 18) {
         result.innerHTML = '$20';
-        reasonContainer.innerHTML = "Adults pay $18 ☺";
+        reasonContainer.innerHTML = "Adults pay $20";
     } else if (ageValue <= 12) {
         result.innerHTML = '$10';
-        reasonContainer.innerHTML = "Children pay $10 ☺";
+        reasonContainer.innerHTML = "Children pay $10";
     } else if (ageValue >= 13 && ageValue <= 17) {
         result.innerHTML = '$15';
-        reasonContainer.innerHTML = "Teenagers pay $15 ☺";
+        reasonContainer.innerHTML = "Teenagers pay $15";
     } else {
         result.innerHTML = '$0';
         reasonContainer.innerHTML = "You are not allowed to enter the cinema :("
