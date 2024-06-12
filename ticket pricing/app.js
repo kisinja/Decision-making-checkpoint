588d41ecf4de5186c391754d1d374fd5b84f2f57
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-06-12 15:37:37 +0300

    Ticket Master: 	new file:   ticket pricing/app.js 	new file:   ticket pricing/index.html 	new file:   ticket pricing/style.css

---
+++ ticket pricing/app.js
@@ -0,0 +1,22 @@
+const age = document.getElementById('age');
+const result = document.getElementById('price-result');
+
+const reasonContainer = document.getElementById("reason-result");
+
+const checkPrice = () => {
+    const ageValue = age.value;
+
+    if (ageValue >= 18) {
+        result.innerHTML = '$20';
+        reasonContainer.innerHTML = "Adults pay $18 ☺";
+    } else if (ageValue <= 12) {
+        result.innerHTML = '$10';
+        reasonContainer.innerHTML = "Children pay $10 ☺";
+    } else if (ageValue >= 13 && ageValue <= 17) {
+        result.innerHTML = '$15';
+        reasonContainer.innerHTML = "Teenagers pay $15 ☺";
+    } else {
+        result.innerHTML = '$0';
+        reasonContainer.innerHTML = "You are not allowed to enter the cinema :("
+    }
+};
